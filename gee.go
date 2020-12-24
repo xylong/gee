7792268afbad4fe31aa5ae8f62e597162f62e0d3
xyl <416319808@qq.com>
2020-12-24 23:47:36 +0800

    gorm

--- gee.go
+++ gee.go
@@ -11,12 +11,15 @@ import (
 type Gee struct {
 	*gin.Engine
 	group *gin.RouterGroup
-	dba   interface{}
+	props []interface{}
 }
 
 // Init 初始化
 func Init() *Gee {
-	gee := &Gee{Engine: gin.New()}
+	gee := &Gee{
+		Engine: gin.New(),
+		props:  make([]interface{}, 0),
+	}
 	gee.Use(ErrorHandler())
 	return gee
 }
@@ -35,13 +38,7 @@ func (gee *Gee) Mount(group string, controllers ...Controller) *Gee {
 	gee.group = gee.Group(group)
 	for _, controller := range controllers {
 		controller.Build(gee)
-		value := reflect.ValueOf(controller).Elem()
-		if value.NumField() > 0 {
-			if gee.dba != nil {
-				value.Field(0).Set(reflect.New(value.Field(0).Type().Elem()))
-				value.Field(0).Elem().Set(reflect.ValueOf(gee.dba).Elem())
-			}
-		}
+		gee.setProp(controller)
 	}
 	return gee
 }
@@ -70,6 +67,33 @@ func (gee *Gee) Attach(middleware Middleware) *Gee {
 
 // DB 设置数据库🔗对象
 func (gee *Gee) DB(dba interface{}) *Gee {
-	gee.dba = dba
+	gee.props = append(gee.props, dba)
 	return gee
+}
+
+// setProp 设置控制器属性
+func (gee *Gee) setProp(controller Controller) {
+	ptr := reflect.ValueOf(controller).Elem()
+	for i := 0; i < ptr.NumField(); i++ {
+		field := ptr.Field(i)
+		// 判断控制器属性是否已经实例化
+		if !field.IsNil() || field.Kind() != reflect.Ptr {
+			continue
+		}
+		// 创建属性
+		if p := gee.getProp(field.Type()); p != nil {
+			field.Set(reflect.New(field.Type().Elem()))
+			field.Elem().Set(reflect.ValueOf(p).Elem())
+		}
+	}
+}
+
+// getProp 获取控制器属性
+func (gee *Gee) getProp(p reflect.Type) interface{} {
+	for _, prop := range gee.props {
+		if p == reflect.TypeOf(prop) {
+			return prop
+		}
+	}
+	return nil
 }
